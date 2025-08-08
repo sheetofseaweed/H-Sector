AzriAnDr/HowlingVoid

[99% MODULAR] Refactors species.eye_icon to head.eye_icon (#2677)
* Refactors species.eye_icon to head.eye_icon

You can do some ungodly things with this, such as putting a Teshari head on a monkey body & it'll still have the correct eye sprite...if you can see them above the cut-off jaw, anyway.

* Apply suggestions from code review

Co-authored-by: Bloop <13398309+vinylspiders@users.noreply.github.com>

---------

Co-authored-by: Bloop <13398309+vinylspiders@users.noreply.github.com>

modular_nova/modules/mutants/code/mutant_zombie_bodyparts.dm
@@ -8,6 +8,7 @@
 	limb_id = SPECIES_MUTANT
 	species_color = "#ffffff"
 	head_flags = HEAD_HAIR|HEAD_LIPS|HEAD_DEBRAIN
+	eyes_icon = 'modular_nova/modules/mutants/icons/mutant_eyes.dmi'
 
 /obj/item/bodypart/chest/mutant_zombie
 	icon_greyscale = 'modular_nova/modules/mutants/icons/mutant_parts_greyscale.dmi'

sheetofseaweed/H-Sector

modular_skyrat/modules/mutants/code/mutant_zombie_bodyparts.dm
@@ -8,6 +8,7 @@
 	limb_id = SPECIES_MUTANT
 	species_color = "#ffffff"
 	head_flags = HEAD_HAIR|HEAD_LIPS|HEAD_DEBRAIN
+	eyes_icon = 'modular_skyrat/modules/mutants/icons/mutant_eyes.dmi'
 
 /obj/item/bodypart/chest/mutant_zombie
 	icon_greyscale = 'modular_skyrat/modules/mutants/icons/mutant_parts_greyscale.dmi'